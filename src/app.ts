cf509d54108e43fdf39c843c7c952d85cbe07e2c
josedasilva123 <55949988+josedasilva123@users.noreply.github.com>
2023-09-08 12:34:24 -0300

    feat: db connection

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
 import "dotenv/config";
 import express from "express";
+import { connectDatabase } from "./database";
 
 const app = express();
 
@@ -8,5 +9,6 @@ app.use(express.json());
 const PORT = process.env.PORT;
 
 app.listen(PORT, async () => {   
+   await connectDatabase();
    console.log(`Server is running on port ${PORT}`);
 });
