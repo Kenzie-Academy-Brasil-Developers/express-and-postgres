e43ece28119bcc381bd8c80506993409bb6702f7
josedasilva123 <55949988+josedasilva123@users.noreply.github.com>
2023-09-12 16:17:25 -0300

    refactor: refactoring get todos

--- src/controllers/todos.controllers.ts
+++ src/controllers/todos.controllers.ts
@@ -1,5 +1,11 @@
 import { Request, Response } from "express";
-import { getTodoById } from "../services/todos.services";
+import { getTodoById, getTodos } from "../services/todos.services";
+
+export const getTodosControler = async (req: Request, res: Response) => {
+    const response = await getTodos();
+    
+    res.status(200).json(response);
+}
 
 export const getTodoByIdController = async (req: Request, res: Response) => {
     const { id } = req.params;
--- src/services/todos.services.ts
+++ src/services/todos.services.ts
@@ -1,6 +1,14 @@
 import { QueryConfig } from "pg";
 import { client } from "../database/database";
 
+export const getTodos = async () => {
+    const query = `SELECT * FROM todos;`;
+ 
+    const data = await client.query(query);
+ 
+    return data.rows;
+ };
+
 export const getTodoById = async (id: string) => {
     const queryString = `SELECT * FROM todos WHERE id = $1;`;
  
