ac9930f145a816ff226f4fa66cd714a679b7b16e
josedasilva123 <55949988+josedasilva123@users.noreply.github.com>
2023-09-12 16:28:50 -0300

    refactor: all todos logic in controller and services

--- src/controllers/todos.controllers.ts
+++ src/controllers/todos.controllers.ts
@@ -1,5 +1,5 @@
 import { Request, Response } from "express";
-import { getTodoById, getTodos } from "../services/todos.services";
+import { createTodo, deleteTodo, editTodo, getTodoById, getTodos } from "../services/todos.services";
 
 export const getTodosControler = async (req: Request, res: Response) => {
     const response = await getTodos();
@@ -8,9 +8,25 @@ export const getTodosControler = async (req: Request, res: Response) => {
 }
 
 export const getTodoByIdController = async (req: Request, res: Response) => {
-    const { id } = req.params;
+    const response = await getTodoById(req.params.id);
+
+    res.status(200).json(response);
+}
+
+export const createTodoController = async (req: Request, res: Response) => {
+    const response = await createTodo(req.body);
+
+    res.status(201).json(response);
+}
+
+export const deleteTodoController = async (req: Request, res: Response) => {
+    const response = await deleteTodo(req.params.id);
     
-    const response = await getTodoById(id);
+    res.status(200).json(response);
+}
+
+export const editTodoController = async (req: Request, res: Response) => {
+    const response = await editTodo(req.params.id, req.body);
 
     res.status(200).json(response);
 }
--- src/services/todos.services.ts
+++ src/services/todos.services.ts
@@ -1,23 +1,68 @@
 import { QueryConfig } from "pg";
 import { client } from "../database/database";
+import { ITodo } from "../interfaces/todos";
+import format from "pg-format";
 
 export const getTodos = async () => {
-    const query = `SELECT * FROM todos;`;
- 
-    const data = await client.query(query);
- 
-    return data.rows;
- };
+   const query = `SELECT * FROM todos;`;
+
+   const data = await client.query(query);
+
+   return data.rows;
+};
 
 export const getTodoById = async (id: string) => {
-    const queryString = `SELECT * FROM todos WHERE id = $1;`;
- 
-    const queryConfig: QueryConfig = {
-       text: queryString,
-       values: [id],
-    };
- 
-    const data = await client.query(queryConfig);
- 
-    return data.rows[0];
- };
+   const queryString = `SELECT * FROM todos WHERE id = $1;`;
+
+   const queryConfig: QueryConfig = {
+      text: queryString,
+      values: [id],
+   };
+
+   const data = await client.query(queryConfig);
+
+   return data.rows[0];
+};
+
+export const createTodo = async (body: Omit<ITodo, "id">) => {
+   const { title, content } = body;
+
+   const queryString = `INSERT INTO todos (title, content)
+     VALUES ($1, $2)
+     RETURNING *;`;
+
+   const queryConfig: QueryConfig = {
+      text: queryString,
+      values: [title, content],
+   };
+
+   const data = await client.query(queryConfig);
+
+   return { message: "Todo created sucessfully", todo: data.rows[0] };
+};
+
+export const deleteTodo = async (id: string) => {
+   const queryString = `DELETE FROM todos WHERE id = $1;`;
+
+   const queryConfig: QueryConfig = {
+      text: queryString,
+      values: [id],
+   };
+
+   await client.query(queryConfig);
+
+   return { message: "Todo was sucessfully deleted." };
+};
+
+export const editTodo = async (id: string, body: Partial<ITodo>) => {
+   const query = format(
+      `UPDATE todos SET (%I) = ROW(%L) WHERE id = (%s) RETURNING *;`,
+      Object.keys(body),
+      Object.values(body),
+      id
+   );
+
+   const data = await client.query(query);
+
+   return { message: "Todo was sucessfully edited.", todo: data.rows[0] };
+};
