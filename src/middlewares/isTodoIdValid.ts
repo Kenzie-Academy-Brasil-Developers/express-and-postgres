5bf896244203baf8f2305b4ee339d59af863edd1
josedasilva123 <55949988+josedasilva123@users.noreply.github.com>
2023-09-13 12:08:20 -0300

    refactor: refactor isTodoIdValid with try catch

--- src/middlewares/isTodoIdValid.ts
+++ src/middlewares/isTodoIdValid.ts
@@ -3,18 +3,27 @@ import { QueryConfig } from "pg";
 import { client } from "../database/database";
 
 export const isTodoIdValid = async (req: Request, res: Response, next: NextFunction) => {
-    const queryString = `SELECT * FROM todos WHERE id = $1`;
+    try {
+        const queryString = `SELECT * FROM todos WHERE id = $1`;
 
-    const queryConfig: QueryConfig = {
-        text: queryString,
-        values: [req.params.id]
-    }
-
-    const foundTodo = await client.query(queryConfig);
+        const queryConfig: QueryConfig = {
+            text: queryString,
+            values: [req.params.id]
+        }
     
-    if(!foundTodo.rows[0]){
-        return res.status(404).json({ error: "Not found any todo with this id" });
-    }
-
-    return next();
+        const foundTodo = await client.query(queryConfig);
+        
+        if(!foundTodo.rows[0]){
+            throw new Error("Not found any todo with this id");
+        }
+    
+        return next(); 
+    } catch (error) {
+        if(error instanceof Error){
+            return res.status(404).json({ error: error });
+        }    
+        
+        console.log(error);
+        return res.status(500).json("Internal server error.");
+    }    
 }
